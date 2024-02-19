b749dd17aeb499afe75c026def6ca1c8468add46
Saroj Shrestha <saroj717@yahoo.com>
2024-02-19 10:57:58 -0700

    chart updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename='/exchange-app'>
       <Routes>
         <Route path="/" exact element={ 
           <div className="container mt-5">
